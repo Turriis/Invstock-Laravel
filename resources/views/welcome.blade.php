6ca2442ab8f7a8515644fb571125636dc67f6c12
Arturo PC escuela <arturocecena@hotmail.com>
2022-10-24 10:40:51 -0600

    SOlamente una prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola
+                    Holaaaaaaaaaaaaaaa
                 </div>
 
                 <div class="links">
